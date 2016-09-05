386cf3e8438036e329f240dd29747cf3406081e0
joker1007 <kakyoin.hierophant@gmail.com>
2016-09-06 00:16:36 +0900

    Remove un used statements

--- ext/given_keyword_args/given_keyword_args.c
+++ ext/given_keyword_args/given_keyword_args.c
@@ -5,8 +5,6 @@ static VALUE
 ruby_given_keyword_args(VALUE self)
 {
     VALUE binding = rb_binding_new();
-    VALUE fname = rb_frame_callee();
-    VALUE mname = rb_funcall(self, rb_intern("__method__"), 0);
     return rb_funcall(self, rb_intern("__given_keyword_args__"), 2, binding, Qfalse);
 }
 
@@ -14,8 +12,6 @@ static VALUE
 ruby_given_keyword_args_with_rest(VALUE self)
 {
     VALUE binding = rb_binding_new();
-    VALUE fname = rb_frame_callee();
-    VALUE mname = rb_funcall(self, rb_intern("__method__"), 0);
     return rb_funcall(self, rb_intern("__given_keyword_args__"), 2, binding, Qtrue);
 }
 
